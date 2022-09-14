e7a4b2714252c241daa3bd08c7684976ad4ba84d
Emrik Hydén <emrikh@kth.se>
2022-09-14 17:15:19 +0200

    Improve image tracking, introduce networking, closes #2, closes #4

--- AR/Assets/Scripts/ObjectInteraction.cs
+++ AR/Assets/Scripts/ObjectInteraction.cs
@@ -11,8 +11,8 @@ public class ObjectInteraction : MonoBehaviour {
 
     // Class that defines ship parts to avoid string references
     static class ShipParts {
-        public const string Body = "Body";
-        public const string Cockpit = "Cockpit";
+        public const string Core = "Core";
+        public const string Engine = "Engine";
         public const string LWing = "LWing";
         public const string RWing = "RWing";
     }
@@ -25,25 +25,15 @@ public class ObjectInteraction : MonoBehaviour {
         if (Input.touchCount <= 0) return; // Return if not touching screen
 
         Touch touch = Input.GetTouch(0); // Only care about first finger
-        Color shipColor;
-
-        // Color should be green when touching segment, white otherwise
-        if (touch.phase == TouchPhase.Began)
-            shipColor = Color.green;
-        else if (touch.phase == TouchPhase.Ended)
-            shipColor = Color.white;
-        else
-            return; // Don't need to change anything
 
         Ray ray = _mainCamera.ScreenPointToRay(touch.position);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit)) { // Check if ray hits object
             switch (hit.collider.transform.name) { // Switch case for different parts of ship
-                case ShipParts.Cockpit:
-                    hit.collider.transform.GetComponent<Renderer>().material.color = Color.yellow; // Change cockpit color to yellow
+                case ShipParts.Core:
+                    
                     break;
                 default:
-                    hit.collider.transform.GetComponent<Renderer>().material.color = shipColor; // Change color
                     break;
             }
         }
