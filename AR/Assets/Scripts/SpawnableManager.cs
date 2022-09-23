7559e8787528d9f6ee4170a8652aa63cb55b9b1a
Emrik Hydén <emrikh@kth.se>
2022-09-23 09:33:38 +0200

    Change environment and ship tracking, #15

---
+++ AR/Assets/Scripts/SpawnableManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SpawnableManager : MonoBehaviour
+{
+    [SerializeField] ARRaycastManager _RaycastManager;
+    List<ARRaycastHit> _Hits = new List<ARRaycastHit>();
+    [SerializeField] GameObject spawnablePrefab;
+
+    [SerializeField] Camera arCam;
+    GameObject spawnedObject;
+
+    private bool notPlaced = true;
+
+    void Start() {
+        spawnedObject = null;
+    }
+
+    void Update() {
+        if (notPlaced && Input.touchCount > 0) {
+            PlaceShip();
+        }
+    }
+
+    private void PlaceShip() {
+        if (_RaycastManager.Raycast(Input.GetTouch(0).position, _Hits)) {
+            if (Input.GetTouch(0).phase == TouchPhase.Began && spawnedObject == null) {
+                spawnablePrefab.transform.position = _Hits[0].pose.position;
+                spawnablePrefab.SetActive(true);
+                notPlaced = false;
+            }
+        }
+    }
+}
