fae760001823357efe555c4818a3b6bf94a0587d
max <maxwippich@hotmail.com>
2022-10-07 14:50:46 +0200

    Alternative joystick controls

--- VR/Assets/Scenes/Interactables/JoystickInteractable.cs
+++ VR/Assets/Scenes/Interactables/JoystickInteractable.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class JoystickInteractable : InteractableScript
 {
     private PlayerController player;
     public Vector3 attitude = Vector3.zero;
 
+    public bool tiltControls = true;
+
+    public float maxPull = 0.1f, maxRot = 75f;
+    private Vector3 initialControllerPosition = Vector3.zero;
+    private Vector2 initialPosVal = Vector2.zero, val = Vector2.zero;
+    public override void Reset()
+    {
+        val = Vector2.zero;
+        transform.localRotation = Quaternion.identity;
+        base.Reset();
+    }
     public void SetPlayer(PlayerController player)
     {
         this.player = player;
@@ -20,6 +32,7 @@ public class JoystickInteractable : InteractableScript
         {
             Debug.LogError("PlayerController not set in joystick.");
         }
+        initialControllerPosition = controller.transform.localPosition;
         return base.Interact(controller);
     }
 
@@ -31,10 +44,47 @@ public class JoystickInteractable : InteractableScript
 
     public override void UpdateInteractable(GameObject controller)
     {
-        
+        if (tiltControls)
+        {
+            TiltControls(controller);
+        } else
+        {
+            PullControls(controller);
+        }
+    }
+
+    private void PullControls(GameObject controller)
+    {
+        float zRot = 0;
+
+        Quaternion relativeRotation = Quaternion.Inverse(controller.transform.localRotation) * initialControllerRotation;
+
+
+        zRot = relativeRotation.eulerAngles.z;
+
+        Vector3 relativePosition = controller.transform.localPosition - initialControllerPosition;
+        Vector3 projectedNS = Vector3.Project(relativePosition, Vector3.forward);
+        Vector3 projectedWE = Vector3.Project(relativePosition, Vector3.left);
+        val.x = Mathf.Clamp((projectedNS.magnitude * Mathf.Sign(Vector3.Dot(projectedNS.normalized, Vector3.forward))) / maxPull + initialPosVal.x, -1, 1);
+        val.y = Mathf.Clamp((projectedWE.magnitude * Mathf.Sign(Vector3.Dot(projectedWE.normalized, Vector3.left))) / maxPull + initialPosVal.y, -1, 1);
+
+        transform.localRotation = Quaternion.Euler(val.x * maxRot, 0f, val.y * maxRot);
+
+        if (zRot > 180)
+        {
+            zRot -= 360;
+        }
+        zRot /= 180.0f;
+
+
+        attitude = new Vector3(-val.x, -zRot, val.y);
+    }
+
+    private void TiltControls(GameObject controller)
+    {
         float xRot = 0;
         float yRot = 0;
-        float zRot = 0;    
+        float zRot = 0;
 
         Quaternion relativeRotation = Quaternion.Inverse(controller.transform.localRotation) * initialControllerRotation;
 
